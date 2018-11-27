f541b8211f8fbec4d375dd350807a927028c439c
nsbinhan <30993685+nsbinhan@users.noreply.github.com>
2018-11-27 22:37:50 +0900

    Update head_injury.py

--- head_injury.py
+++ head_injury.py
@@ -78,6 +78,8 @@ model.summary()
 history = model.fit(tX_train, y_train, batch_size=50, epochs=100, verbose=1)
 
 # illustrate how loss and accuracy
+import matplotlib.pyplot as plt
+
 plt.plot(history.history['loss'], '--b')
 plt.plot(history.history['accuracy'], '--r')
 plt.title('training loss/accuracy')
