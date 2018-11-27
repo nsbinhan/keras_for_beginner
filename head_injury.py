de49330ed0bf95d0599976bd0dd40470f63880e3
nsbinhan <30993685+nsbinhan@users.noreply.github.com>
2018-11-27 21:46:57 +0900

    Update head_injury.py

--- head_injury.py
+++ head_injury.py
@@ -1,3 +1,48 @@
+'''
+Objective: Use logistic regression model to match roughly the clinical characteristics of a 
+sample of individuals who suffered minor head injuries
+
+Link: 
+http://vincentarelbundock.github.io/Rdatasets/doc/DAAG/head.injury.html
+http://vincentarelbundock.github.io/Rdatasets/csv/DAAG/head.injury.csv
+
+This data frame contains the following columns:
+
+age.65
+age factor (0 = under 65, 1 = over 65).
+
+amnesia.before
+amnesia before impact (less than 30 minutes = 0, more than 30 minutes =1).
+
+basal.skull.fracture
+(0 = no fracture, 1 = fracture).
+
+GCS.decrease
+Glasgow Coma Scale decrease (0 = no deterioration, 1 = deterioration).
+
+GCS.13
+initial Glasgow Coma Scale (0 = not ‘13’, 1 = ‘13’).
+
+GCS.15.2hours
+Glasgow Coma Scale after 2 hours (0 = not ‘15’, 1 = '15').
+
+high.risk
+assessed by clinician as high risk for neurological intervention (0 = not high risk, 1 = high risk).
+
+loss.of.consciousness
+(0 = conscious, 1 = loss of consciousness).
+
+open.skull.fracture
+(0 = no fracture, 1 = fracture)
+
+vomiting
+(0 = no vomiting, 1 = vomiting)
+
+clinically.important.brain.injury
+any acute brain finding revealed on CT (0 = not present, 1 = present).
+'''
+
+################################################################################################
 # Step 1: Extract features
 
 # Read raw data
